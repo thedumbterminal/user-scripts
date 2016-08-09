bf2f2032a96ff71d0db350891cd57d7941f0e6ab
thedumbterminal <github@thedumbterminal.co.uk>
2016-08-09 21:20:18 +0100

    New script to remove facebook suggested posts

---
+++ removeFacebookSuggestedPosts.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name        removeFacebookSuggestedPosts
+// @namespace   DT
+// @include     https://www.facebook.com/
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+var DT = {};
+
+DT._ticking = false;
+
+DT.main = function(){
+	var spans = document.getElementsByTagName("SPAN");
+	var totalSpans = spans.length;
+	for(var i = 0; i < totalSpans; i++){
+		var item = spans.item(i);
+		if(item && item.textContent === 'Suggested Post'){
+			var parent = item.parentNode.parentNode.parentNode.parentNode.parentNode;
+			if(parent){
+				parent.parentNode.removeChild(parent);
+				console.log('Removed suggested post');
+			}
+		}
+	}
+};
+
+DT._tick = function(){
+	if(!DT._ticking){
+		window.setTimeout(function(){
+			DT.main();
+			DT._ticking = false;
+		}, 3000);
+	}
+	DT._ticking = true;
+};
+
+document.addEventListener("DOMContentLoaded", DT._tick);
+window.addEventListener('scroll', DT._tick);
